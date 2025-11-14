d4fce0d0c94db4f797f7d317d8b2cbdd2ebc3c16
Tyler Mowen <tylrmwn012@gmail.com>
2025-11-14 13:24:20 -0600

    Switch Potentiometer to Rotary Module

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Arduino-Arm-Project
+
+This project utilizes an Arduino board, servos, and potentiometers
+to form a functioning arm with 4 pivot points, and a gripper.
+
+The goal of this side project is to expand my knowledge on embedded 
+programming, while also dive into a project that can be improved upon
+over time.
--- arm-project.ino
+++ arm-project.ino
@@ -1,36 +1,47 @@
-// this is my initial ARM project
+// demo code which takes input from rotary encoder and outputs to servo
 #include <Servo.h>
+Servo base;
 
-Servo shoulderOne;
-Servo shoulderTwo;
-Servo elbow;
-Servo wristOne;
-Servo wristTwo;
-Servo gripper;
+const int CLK = 2; // encoder pin A
+const int DT = 3; // encoder pin B
+
+int counter = 90; // initial position at 90 degrees
+
+int currStateCLK; // current state of A
+int lastStateCLK; // last state of A
+int servoPos = 90;
 
 void setup() {
-  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(CLK, INPUT); // set pin as input
+  pinMode(DT, INPUT); // set pin as input
+  // unecessary
+  Serial.begin(9600); // open serial channel
 
-  shoulderOne.attach(9);
-  shoulderTwo.attach(8);
-  elbow.attach(7);
-  wristOne.attach(6);
-  wristTwo.attach(5);
-  gripper.attach(4);
+  base.attach(9); // attach servo to pin 9
+  base.write(servoPos); // set initial angle of servo
+
+  lastStateCLK = digitalRead(CLK);
 }
 
+// if 1 -> steps 5
+// if 0 -> nothing
 void loop() {
-  int shoulderValue = analogRead(A1);
-  int elbowValue = analogRead(A2);
-  int wristOneValue = analogRead(A3);
-  int wristTwoValue = analogRead(A4);
-  int gripperValue = analogRead(A5);
+  currStateCLK = digitalRead(CLK); // set curr as actual current position
 
-  shoulderOne.write((shoulderValue * 180 + 511) / 1023);
-  shoulderTwo.write(-(shoulderValue * 180 + 511) / 1023);
-  elbow.write((elbowValue * 180 + 511) / 1023);
-  wristOne.write((wristOneValue * 180 + 511) / 1023);
-  wristTwo.write((wristTwoValue * 180 + 511) / 1023);
-  gripper.write((gripperValue * 180 + 511) / 1023);
-  delay(1);
+  if (currStateCLK != lastStateCLK && currStateCLK == 1) { 
+    if (digitalRead(DT) != currStateCLK) {
+      counter -= 5; // decrement by 5
+    } else { 
+      counter += 5; // increment by 5
+    }
+
+    servoPos = constrain(counter, 0, 180); 
+    base.write(servoPos); 
+    // unecessary
+    Serial.print("Servo Angle: "); // display servo's angle
+    Serial.println(servoPos); // display servo's angle
+  }
+
+  lastStateCLK = currStateCLK; // set equal to show they haven't moved
 }
